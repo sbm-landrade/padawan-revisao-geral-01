69072c66b2ef982febecc027622d893ee5c17794
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-08-01 09:48:32 -0300

    Criando interface para buscar nomes com JPA

--- futebol-api-backend/futebol/src/main/java/br/com/api/futebol/repository/JogadorRepository.java
+++ futebol-api-backend/futebol/src/main/java/br/com/api/futebol/repository/JogadorRepository.java
@@ -1,8 +1,11 @@
 //Interface de repositório JPA para operações CRUD.
 package br.com.api.futebol.repository;
 
+import java.util.List;
 import br.com.api.futebol.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JogadorRepository extends JpaRepository<Jogador, Long> {
+
+	public List<Jogador> findByNome(String name);
 }
