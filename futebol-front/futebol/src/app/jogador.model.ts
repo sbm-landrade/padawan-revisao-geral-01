7f034f50c15a7640ade9979a05aaf9c5d328cad9
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-08-01 22:27:45 -0300

    Exclusão de pastas

--- futebol-front/futebol/src/app/jogador.model.ts
+++
@@ -1,6 +0,0 @@
-//define a estrutura do objeto Jgadores, garantindo que os objetos jogadores funcionam de forma esperada
-export interface Jogador {
-  id?: number;
-  nome: string;
-  time: string;
-}
